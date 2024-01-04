5668eea93dc9e6c0271849da2f1e0ba041550d80
zivdvash <zivdvash@gmail.com>
2024-01-04 21:40:53 +0200

    factorial

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import math
+
+
+def factorial(n: float):
+    if (n == 1):
+        return 1
+
+    return n * factorial(n - 1)
 
 
 
+def main():
+    print(factorial(5))
+
+
+if __name__ == "__main__":
+    main()
